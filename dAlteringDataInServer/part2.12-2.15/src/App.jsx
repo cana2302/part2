f5e639bd05b86006f2f56bf8118dcd8d2a490783
Franco Canavese <fcanavese@hotmail.com>
2024-03-28 00:03:57 +0100

    Exercise 2.13.

--- dAlteringDataInServer/part2.12-2.15/src/App.jsx
+++ dAlteringDataInServer/part2.12-2.15/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter'
+import personService from './services/persons';
 
 const App = () => {
 
@@ -13,10 +14,10 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -31,12 +32,14 @@ const App = () => {
       setPersons(persons.concat(personObject));
       setNewName('');
       setNewNumber('');
+      
+      personService
+      .create(personObject)
+        .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+      })
 
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          console.log(response.data)
-        })
     }
   }
 
